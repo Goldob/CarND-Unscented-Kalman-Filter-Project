a0a0b65a0a909e2f6af44669acd816ad16abb6de
Adam Gotlib <gotlib.adam+dev@gmail.com>
2019-01-28 19:14:02 +0100

    Calculate RMSE from predictions

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,7 +9,17 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+   int num_measurements = estimations.size();
+   int num_variables = estimations[0].size();
+   VectorXd residuals = VectorXd::Zero(num_variables);
+
+   for (int i = 0; i < num_variables; i++) {
+     for (int j = 0; j < num_measurements; j++) {
+       residuals[i] += pow(estimations[j][i] - ground_truth[j][i], 2);
+     }
+
+     residuals[i] = sqrt(residuals[i] / num_measurements);
+   }
+
+   return residuals;
 }
